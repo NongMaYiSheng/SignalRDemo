ac02fb30b40176d28a6f09b6825883fe64f3d908
mcgrady525 <mcgrady_525@163.com>
2017-07-19 18:38:16 +0800

    add

---
+++ SignalRDemo.MVC.Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRDemo.MVC.Site.MvcApplication" Language="C#" %>
---
+++ SignalRDemo.Site/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRDemo.Site
+{
+    /// <summary>
+    /// 聊天室Hub
+    /// </summary>
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            //Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+
+            //推送消息到指定客户端
+            //var connectionId = "";
+            //Clients.Client(connectionId).broadcastMessage(name, message);
+
+        }
+    }
+}
